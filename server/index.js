f64445fd6d3945a39ecc17ada6162a8e1a1e7c3e
apurvpatil18 <patilapurv18@gmail.com>
2023-06-22 10:08:04 +0530

    added the folder into git

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+
+/* CONFIGURATION */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+
+/* SERVER SETUP */
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(
+    "mongodb+srv://patilapurv18:wKkLUMvAjqGyxN08@cluster0.tkgl6tg.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed:", error);
+  });
